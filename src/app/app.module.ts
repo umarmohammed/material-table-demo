1dece79ae8ec71de3d0d90b0aa9b88e06d6d1454
Umar Mohammed <umar@teacherapp.co.uk>
2019-03-17 17:18:24 +0000

    show cdk table
    #20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatTableModule } from '@angular/material/table';
-import { DemoMatTableComponent } from './components/demo-mat-table/demo-mat-table.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ScrollDispatchModule } from '@angular/cdk/scrolling';
+
+import { DemoMatTableComponent } from './components/demo-mat-table/demo-mat-table.component';
+import { DemoCdkScrollComponent } from './components/demo-cdk-scroll/demo-cdk-scroll.component';
+
+const routes: Routes = [
+  { path: 'material-table', component: DemoMatTableComponent },
+  { path: 'cdk-scroll', component: DemoCdkScrollComponent },
+  { path: '', redirectTo: 'material-table', pathMatch: 'full' }
+];
+
 @NgModule({
-  declarations: [AppComponent, DemoMatTableComponent],
+  declarations: [AppComponent, DemoMatTableComponent, DemoCdkScrollComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatTableModule,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule
+    ScrollDispatchModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
